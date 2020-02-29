3c1bd98ac6582179a26158fb8e146772227583eb
unknown <you@example.com>
2020-02-29 16:07:01 +0800

    下载element组件

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-
+import ElementUI from 'element-ui' // 引入模块
 Vue.config.productionTip = false
+Vue.use(ElementUI) // 全局注册element-ui框架 一旦全局注册 所有的位置都可以使用组件
 
 new Vue({
   router,
