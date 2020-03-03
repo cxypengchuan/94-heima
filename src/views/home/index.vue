efe281e8a44cfe0b6b0726ae3a123bc9be1fea0b
unknown <you@example.com>
2020-03-03 14:26:57 +0800

    登录校验并跳转

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,5 +1,22 @@
 <template>
-  <div>主页</div>
+   <el-container>
+     <el-aside>
+       <h1>左侧导航区域</h1>
+     </el-aside>
+     <!-- 右侧 再嵌套一个 el-container -->
+     <el-container>
+       <!-- 再上下布局 -->
+       <el-header>
+         <h1>顶部操作栏</h1>
+       </el-header>
+       <!-- 主要区域 -->
+       <el-main>
+         <h1>主要区域</h1>
+          <!-- 这里应该放置 二级路由容器 -->
+         <router-view></router-view>
+       </el-main>
+     </el-container>
+   </el-container>
 </template>
 
 <script>
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -21,7 +21,7 @@
             </el-form-item>
             <!-- 登陆按钮 -->
             <el-form-item style='margin-top:20px'>
-           <el-button   style="width:100% " type="primary" >登录</el-button>
+           <el-button   style="width:100% " type="primary" @click="login" >登录</el-button>
             </el-form-item>
 
         </el-form>
@@ -42,12 +42,24 @@ export default {
       // 定义检验规则
       loginRules: {
         // required是检验必填项
-        mobile: [{ required: true, message: '请输入您的电话号码' },
+        mobile: [{
+          required: true,
+          message: '请输入您的电话号码'
+        },
         // pattren检验内容的格式
-          { pattern: /^1(3|4|5|6|7|8|9)\d{9}$/, message: '您的手机号格式不正确' }
+        {
+          pattern: /^1[3-9]\d{9}$/,
+          message: '您的手机号格式不正确'
+        }
         ],
-        code: [{ required: true, message: '请输入验证码' },
-          { pattern: /^\d{6}$/, message: '验证码输入错误' }],
+        code: [{
+          required: true,
+          message: '请输入验证码'
+        },
+        {
+          pattern: /^\d{6}$/,
+          message: '验证码输入错误'
+        }],
         checked: [{
           validator: function (rule, value, callback) {
             // rule是当前的校验规则
@@ -60,6 +72,25 @@ export default {
           }
         }]
       }
+    }
+  },
+  methods: {
+    login () {
+      // 校验通过后执行then后面的函数
+      this.$refs.loginForm.validate().then(() => {
+        this.$axios({
+          url: '/authorizations',
+          data: this.loginForm,
+          method: 'post'
+          // 请求成功后把token值保存在本地数据库中
+        }).then(res => {
+          window.localStorage.setItem('user-token', res.data.data.token)
+          // 保存后跳转回主页
+          this.$router.push('/home')
+        }).catch(() => {
+          this.$message.error('用户名或密码错误')
+        })
+      })
     }
   }
 }
