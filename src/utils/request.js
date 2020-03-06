60a12cdf67940c8343a68b7ccf705dc1fdc02fb0
unknown <you@example.com>
2020-03-06 19:48:52 +0800

    大数字处理

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,6 +1,17 @@
 import axios from 'axios'
 import router from '@/router'
+import JSONBig from 'json-bigint' // 引入地方处理大数字的包
 axios.defaults.baseURL = ('http://ttapi.research.itcast.cn/mp/v1_0') // 配置公共的请求头地址
+// 处理大数字
+axios.defaults.transformResponse = [function (data) {
+  // 这里主要处理 id 超过 大数字的时候  转化 不正确的问题  JSONBig.parse是第三方的包 内部怎么实现的,我们不需要关心
+  // 只需要知道它可以帮助我们 转化正确
+  // const result = JSON.parse(data)
+  // return result
+  // 用json-bigint的转化来做
+  // 这里需要判断一下 data 是不是为空 如果为空就不能转化了
+  return data ? JSONBig.parse(data) : {}
+}]
 // 请求拦截器开发
 axios.interceptors.request.use(function (config) {
   const token = localStorage.getItem('user-token')// 在缓存中拿到token
--- src/views/comment/index.vue
+++ src/views/comment/index.vue
@@ -20,6 +20,7 @@
           </template>
    </el-table-column>
 </el-table>
+
 </el-card>
 </template>
 
@@ -50,11 +51,40 @@ export default {
       }).then((res) => {
         this.list = res.data.results
       })
+    },
+    openOrClose (row) {
+      const mess = row.comment_status ? '关闭' : '打开'
+      // $confirm 也支持 promise 点击确定会进入到then 点击取消会进入到catch
+      this.$confirm(`是否确定${mess}评论`, '提示').then(() => {
+      // 调用打开或者关闭接口
+        this.$axios({
+          url: '/comments/status', // 请求地址
+          method: 'put', // 请求类型
+          // query参数
+          params: {
+          // 为什么评论会失败 就是因为 原来 给你传了 9152 你回传了 9200
+          // 所以我们用大数字包 保证 9152不被转化 就可以使用原来的功能
+            article_id: row.id.toString() // 要求参数的文章id 将 BigNumber类型转化成字符串
+          // 前端传字符串到后端 只要和原数字一致  后端会自动将字符串转成大数字
+          // 只需要保证 id 和传过来的id一致就行
+          },
+          data: {
+          //  body参数
+            allow_comment: !row.comment_status // 是打开还是关闭  此状态和评论状态相反
+          }
+        }).then(() => {
+        //   成功了 提示个消息 然后 重新拉取数据4
+          this.$message.success(`${mess}评论成功`)
+          //  重新拉取数据
+          this.getList() // 调用重新拉取数据的方法
+        }).catch(() => {
+        //   表示失败了 会进入到catch
+          this.$message.error(`${mess}评论失败`)
+        })
+      })
     }
   },
-  openOrClose (row) {
 
-  },
   created () {
     this.getList()
   }
