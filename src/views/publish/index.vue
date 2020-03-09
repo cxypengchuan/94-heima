e976b39c8dc53dee8f7e00cb94b3bafc0248af42
unknown <you@example.com>
2020-03-09 22:08:38 +0800

    富文本编辑器

--- src/views/publish/index.vue
+++ src/views/publish/index.vue
@@ -12,9 +12,10 @@
         </el-form-item>
         <el-form-item label="内容" prop="content">
           <!-- 多行输入 -->
-          <el-input placeholder="请输入您的内容" type='textarea' :rows="4" v-model="publishForm.content"></el-input>
+          <!-- <el-input placeholder="请输入您的内容" type='textarea' :rows="4" v-model="publishForm.content"></el-input> -->
+        <quill-editor v-model="publishForm.content" style="height:300px"></quill-editor>
         </el-form-item>
-        <el-form-item label="封面" prop="cover">
+        <el-form-item label="封面" prop="cover" style="margin-top:120px">
           <!-- 单选框组 -->
           <el-radio-group v-model="publishForm.cover.type">
              <el-radio :label="1">单图</el-radio>
